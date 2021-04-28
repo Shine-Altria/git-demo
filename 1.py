bef73c42dc1ce9fb2e7ce291859b2910002e20b1
Shine-Altria <461613583@qq.com>
2021-04-28 11:29:25 +0800

    first

--- 1.py
+++ 1.py
@@ -1,2 +1,4 @@
 a=2
 b=1
+
+# feel happy i can use git hub!
--- git-use.py
+++ git-use.py
@@ -1,14 +1,45 @@
-# git use mothod
-git config --global user.name "Shine"
-git config --global user.email "461613583@qq.com"
-git init
-open .git
-git status
-git add
-git log
+# git use method
+# git config --global user.name "Shine"
+# git config --global user.email "461613583@qq.com"
+# git init
+# open .git
+# git status
+# git add 1.py
+# git log -oneline
+# git commit -m "asdasjdasj"
+# git add .
+# git status -s
+#
+# git diff
+# git diff --cached
 
+# git reset  相对应   git add    Modified 相对 Staged
+# git reset -hard HEAD    回到当前版本
+# git reset -hard HEAD^  回到上一个版本
+# git reset -hard XXXXXXXX  回到过去的地址版本
+# git reset reflog 全部变化
 
+# git checkout  XXXX地址 -- 1.py 单个文件回到某一个版本
+# git branch
+# git checkout dev 切换新分支
+# git branch -d 删除分支
+# git checkout -b dev 建立新分支
 
+# git commit -am "chanasdgjfjdk33"
+
+# git merge --no-ff -m "keep merge info" dev
+# git log -oneline -graph
+
+# git merge dev
+# git commit -am "solve conflict"
+
+# git rebase dev
+# git rebase --continue
+# git rebase --abort
+
+# git stash
+# git checkout dev
+# git stash pop
 
 
 
